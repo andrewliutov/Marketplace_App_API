f5ac89b1b8bf17b98041da5420cbb49793efe4b5
Andrew Liutov <87426517+andrewliutov@users.noreply.github.com>
2024-06-22 01:30:26 +0300

    Update settings.py

--- market/settings.py
+++ market/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=9^s0m(@p!8jvqhu0spme)6smnlzj=g=(q$#3-h&du4#dm&7k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diplom_db',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
